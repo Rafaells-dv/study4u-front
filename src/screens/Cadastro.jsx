af59fe233b90bbe3591c1fbf32cb5068f9283937
Rafael Leonardo <rafaelleonardodasilva.dv@gmail.com>
2024-04-29 21:15:29 -0300

    adicionar formulário de cadastro de usuario

--- src/screens/Cadastro.jsx
+++ src/screens/Cadastro.jsx
@@ -1,9 +1,18 @@
 import React from "react";
+import "./Cadastro.css";
 
 function Cadastro() {
     return (
-        <div>
-            <p>Cadastro</p>
+        <div id="cadastro">
+            <form id="cadastro-form">
+                <p className="title">Cadastre-se</p>
+                <div id="cadastro-inputs">
+                    <input className="text" type="text" name="name" placeholder="Nome:"/>
+                    <input className="text" type="email" name="email" placeholder="Email:"/>
+                    <input className="text" type="password" name="password" placeholder="Senha:"/>
+                    <input className="text" type="submit" name="cadastrar" value="Cadastrar"/>
+                </div>
+            </form>
         </div>
     )
 }
--- src/screens/Login.jsx
+++ src/screens/Login.jsx
@@ -4,7 +4,7 @@ import "./Login.css";
 function Login() {
     return (
     <main>
-        <form>
+        <form id="login-form">
             <article className="title">
                 <p id="login-title">Study4U</p>
                 <p className="text">Comece a mudar o seu futuro.</p>
@@ -13,7 +13,7 @@ function Login() {
                 <p className="title">Login</p>
                 <input className="text" type="email" name="email" placeholder="Email:"/>
                 <input className="text" type="password" name="password" placeholder="Senha:"/>
-                <input className="text" type="submit" value="Logar"/>
+                <input className="text" type="submit" name="logar" value="Logar"/>
                 <div className="text">
                     <a href="">Esqueceu sua senha?</a>
                     <a href="">Cadastrar</a>
