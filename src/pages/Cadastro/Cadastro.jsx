d84e4ac915cfba2de379f080f3bb80c14c100bfc
Rafael <rafaelleonardodasilva@gmail.com>
2024-05-10 19:40:28 -0300

    adicionar request de registro de usuario

--- src/pages/Cadastro/Cadastro.jsx
+++ src/pages/Cadastro/Cadastro.jsx
@@ -1,16 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Cadastro.css";
 
 function Cadastro() {
+
+    const [form, setForm] = useState({name: '', email: '', password: ''});    
+    
+    function handleChange(e) {
+        setForm({...form, [e.target.name]: e.target.value});
+    }
+    
+    const createUser = async (form) => {
+        const request = await fetch('http://localhost:8080/usuarios', {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({
+                email: form.email,
+                nome: form.name,
+                senha: form.password
+              })
+        })
+
+        if (request.status >= 200 && request.status <= 299) {
+            alert("usuário criado com sucesso")
+            navigate("/login")
+            return
+          }
+        }
+    }
+
+
+
+
+
     return (
         <>
             <main>
                 <form id="cadastro-form">
                     <p className="title">Cadastre-se</p>
                     <div id="cadastro-inputs">
-                        <input className="text" type="text" name="name" placeholder="Nome:"/>
-                        <input className="text" type="email" name="email" placeholder="Email:"/>
-                        <input className="text" type="password" name="password" placeholder="Senha:"/>
+                        <input className="text" type="text" name="name" placeholder="Nome:" onChange={handleChange}/>
+                        <input className="text" type="email" name="email" placeholder="Email:" onChange={handleChange}/>
+                        <input className="text" type="password" name="password" placeholder="Senha:" onChange={handleChange}/>
                         <input className="text" type="submit" name="cadastrar" value="Cadastrar"/>
                     </div>
                 </form>
