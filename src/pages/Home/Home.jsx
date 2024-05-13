5d6d555a27ac928c0df1b798a83a5eba15f9c52a
Rafael Leonardo <rafaelleonardodasilva.dv@gmail.com>
2024-05-13 09:35:21 -0300

    adicionar requisição de pesquisar salas no Home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,15 +1,47 @@
-import React from "react";
+import React, {useContext} from "react";
 import Sidebar from "../../components/Sidebar.jsx"
 import Salas from "../../components/Salas.jsx";
 import "./Home.css"
+import { UserContext } from "../../contexts/UserContext.jsx";
 
 function Home() {
+
+    const {setUser, user} = useContext(UserContext)
+
+    const [search, setSearch] = useState('');
+
+    function handleChange(event) {
+        setSearch(event.target.value)
+
+        const searchClasses = async (event) => {
+            try {
+                const request = await fetch(`http://localhost:8080/pesquisar-titulo?pesquisa=${search}`, {
+                    method: "GET",
+                    headers: { 
+                        "Content-Type": "application/json",
+                        Authorization: 'Bearer' + user.token,
+                    },
+                });
+    
+                if(request.ok) {
+                    const data = await request.json(); // Transforma a resposta em JSON
+                    console.log(data)
+                }
+            } catch(error) {
+                console.log('Erro ao pesquisar salas', error)
+            }
+        }
+        
+        console.log(searchClasses)
+    }
+
+
     return (
         <>
         <div id="private">
             <Sidebar />
             <div id="home">
-                <input type="search" className="text" name="pesquisar" placeholder="Pesquisar salas..."/>
+                <input type="search" className="text" name="pesquisar" placeholder="Pesquisar salas..." onChange={handleChange}/>
                 <div className="grupo-salas">
                     <Salas titulo="Matemática 2° ano" desc="Sala para estudos de mátemática do 2° ano do médio"/>
                     <Salas titulo="Matemática 2° ano" desc="Sala para estudos de mátemática do 2° ano do médio"/>
